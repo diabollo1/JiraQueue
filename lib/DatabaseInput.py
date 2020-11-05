b9d06b6439d4b4a6c1b74ff2857e65a5e345e584
Tomasz <tomasz@kulinowski.pl>
2020-11-05 16:59:31 +0100

    added error handling and logging

--- lib/DatabaseInput.py
+++ lib/DatabaseInput.py
@@ -1,7 +1,11 @@
+import json
+import sys
+from aifc import Error
+
 import mysql.connector
 from mysql.connector import Error
 
-from conf import db_conf
+from conf import db_conf, log_conf
 
 
 class DatabaseInput:
@@ -12,7 +16,13 @@ class DatabaseInput:
         self.password = db_conf.password
         self.db_table_name = db_conf.db_table_name
 
+        self.logger = log_conf.get_logger(__class__.__name__ + ".log")
+
         self.connection = self.create_connection()
+        # if not self.connection:
+        #     raise Exception("db connection fail")
+
+        # print()
 
     def create_connection(self):
         connection = None
@@ -23,36 +33,55 @@ class DatabaseInput:
                 passwd=self.password,
                 database=self.db_name
             )
-            print("Connection to MySQL DB successful")
+            self.logger.debug("Connection to MySQL DB (" + self.host + ":" + self.db_name + ") successful")
+            return connection
         except Error as e:
-            # TODO: Error handling
-            print("The error occurred" + str(e))
-        return connection
+            self.logger.error("The error occurred: " + str(e))
+            raise Exception(__name__ + ":" + str(sys.exc_info()[2].tb_lineno) + "|" + str(e))
 
     def get_all_issue_list(self):
-        mycursor = self.connection.cursor()
-
-        mycursor.execute("SELECT * FROM "+self.db_name+"."+self.db_table_name)
-
-        myresult = mycursor.fetchall()
-        return myresult
+        try:
+            mycursor = self.connection.cursor()
+            sql = "SELECT * FROM "+self.db_name+"."+self.db_table_name
+            self.logger.debug(sql)
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            return myresult
+        except Error as e:
+            self.logger.error("The error occurred: " + str(e))
+            raise Exception(__name__ + ":" + str(sys.exc_info()[2].tb_lineno) + "|" + str(e))
 
     def get_issue_to_create_list(self):
-        mycursor = self.connection.cursor()
-
-        mycursor.execute("SELECT * FROM "+self.db_name+"."+self.db_table_name+" WHERE issue_date_adding is null")
-
-        myresult = mycursor.fetchall()
-        return myresult
+        try:
+            mycursor = self.connection.cursor()
+            sql = "SELECT * FROM "+self.db_name+"."+self.db_table_name+" WHERE issue_date_adding is null"
+            self.logger.debug(sql)
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            if myresult:
+                self.logger.debug("Successful get " + str(len(myresult)) + " issue from db")
+                return myresult
+            else:
+                self.logger.info("No issue to create")
+                raise Exception(__name__ + "|No issue to create")
+        except Error as e:
+            self.logger.error("The error occurred: " + str(e))
+            raise Exception(__name__ + ":" + str(sys.exc_info()[2].tb_lineno) + "|" + str(e))
 
     def set_data_issue(self, list_db_id, create_issue_output):
-        mycursor = self.connection.cursor()
-        # TODO: complete the rest fields (issue_id,issue_key,issue_url) = create_issue_output
-        sql = "UPDATE " + self.db_name + "." + self.db_table_name + \
-              " SET issue_date_adding = CURRENT_TIMESTAMP WHERE id in (" + list_db_id + ")"
-        print(sql)
-        mycursor.execute(sql)
+        try:
+            mycursor = self.connection.cursor()
+            # TODO: complete the rest fields (issue_id,issue_key,issue_url) = create_issue_output
+            sql = "UPDATE " + self.db_name + "." + self.db_table_name + \
+                  " SET issue_date_adding = CURRENT_TIMESTAMP WHERE id in (" + list_db_id + ")"
+            self.logger.info(sql)
+            mycursor.execute(sql)
 
-        self.connection.commit()
-        return mycursor.rowcount, "record(s) affected"
+            self.connection.commit()
+            self.logger.info("Successful update " + mycursor.rowcount + " issue in db")
+            return mycursor.rowcount, "record(s) affected"
+        except Error as e:
+            self.logger.error("The error occurred: " + str(e))
+            raise Exception(__name__ + ":" + str(sys.exc_info()[2].tb_lineno) + "|" + str(e))
 
+    # TODO: function for all database operations
--- test_get_db_create_jira_issue.py
+++ test_get_db_create_jira_issue.py
@@ -1,16 +1,17 @@
+import sys
+
 from lib.DatabaseInput import DatabaseInput
 from lib.JiraIssue import JiraIssue
 
-db = DatabaseInput()
-issues = db.get_issue_to_create_list()
+try:
+    db = DatabaseInput()
+    issues = db.get_issue_to_create_list()
 
-print(issues)
-
-if issues:
+    print(issues)
 
     data = {"id": "", "json": {"issueUpdates": []}}
     for issue in issues:
-        data["id"] += str(issue[0])+","
+        data["id"] += str(issue[0]) + ","
         data["json"]["issueUpdates"].append(eval(issue[3]))
 
     data["id"] = data["id"].rstrip(',')
@@ -21,6 +22,7 @@ if issues:
     create_issue_output = i1.create_issue(data["json"])
     print(create_issue_output)
 
-    print(db.set_data_issue(data["id"],create_issue_output))
-else:
-    print("nope!")
+    print(db.set_data_issue(data["id"], create_issue_output))
+
+except Exception as e:
+    print("The error occurred: " + __name__ + ":" + str(sys.exc_info()[2].tb_lineno) + "|" + str(e))
